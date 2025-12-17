1a4120cb8acac5dad677c300d310ab3c4a39b4ae
daivy <2259492701@qq.com>
2025-12-17 22:09:11 +0800

    2

--- code/os/common.mk
+++ code/os/common.mk
@@ -7,10 +7,10 @@ include ../defines.mk
 DEFS +=
 
 CROSS_COMPILE = riscv64-unknown-elf-
-CFLAGS += -nostdlib -fno-builtin -g -Wall
-CFLAGS += -march=rv32g -mabi=ilp32
+CFLAGS += -nostdlib -fno-builtin -g -Wall -mcmodel=medany -fno-pic
 
-QEMU = qemu-system-riscv32
+
+QEMU = qemu-system-riscv64
 QFLAGS = -nographic -smp 1 -machine virt -bios none
 
 GDB = gdb-multiarch
